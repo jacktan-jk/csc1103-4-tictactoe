79c3464bb30fc5a15173c6fc79ec514d3b3592b8
itsamekai <resayz4@ymail.com>
2024-10-30 16:28:26 +0800

    added the random data splitting of dataset (random 80/20) and write into two files respectively, training and testing.

--- src/ml-naive-bayes.c
+++ src/ml-naive-bayes.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define DATA_SIZE 958
+// changed to 100 for testing. make sure to chg back
+#define DATA_SIZE 100
 #define CLASSES 2
+
 
 struct Dataset {
     char grid[3][3];
@@ -11,6 +14,12 @@ struct Dataset {
 };
 
 struct Dataset data[DATA_SIZE];
+
+// to write to directory before 
+const char *trainingFile = "../training-tic-tac-toe.data";
+const char *testingFile = "../testing-tic-tac-toe.data";
+
+int randomNo[DATA_SIZE];
 
 // Counters for positive and negative outcomes
 int positive_count = 0;
@@ -34,6 +43,71 @@ int assignMoveIndex(char move) {
     }
 }
 
+void getRandomNo(int random[DATA_SIZE]) {
+    int count = 0;
+    srand(time(NULL));
+
+    // initialize all to 0 for proper check
+    int check[DATA_SIZE];
+    for (int i = 0; DATA_SIZE > i; i++) {
+        check[i] = 0;
+    } 
+
+    while (DATA_SIZE > count) {
+        int randNo = rand() % DATA_SIZE;
+        if (check[randNo] == 0) {
+            check[randNo] = 1;
+            random[count] = randNo;
+            count++;
+        }
+    }
+}  
+
+
+// split into 80 - 20; write into training and testing file accordingly
+void splitFile() {
+    // get 80% and 20% respectively
+    int eighty = 0.8 * DATA_SIZE;
+    int twenty = 0.2 * DATA_SIZE;
+
+    // write into training dataset
+    FILE *trainFile;
+    trainFile = fopen(trainingFile, "w");
+    if (!trainFile) {
+        printf("Error opening file. \n");
+    }
+
+    for (int i = 0; eighty > i; i++) {
+        for (int row = 0; 3 > row; row++) {
+            for (int col = 0; 3 > col; col++) {
+                fprintf(trainFile, "%c,", data[i].grid[row][col]);
+            }
+        }
+        fprintf(trainFile, "%s\n", data[i].outcome);
+    }
+
+    fclose(trainFile);
+
+    // write into testing dataset
+    FILE *testFile;
+    testFile = fopen(testingFile, "w");
+    if (!testFile) {
+        printf("Error opening file. \n");
+    }
+
+    for (int i = eighty; DATA_SIZE > i; i++) {
+        for (int row = 0; 3 > row; row++) {
+            for (int col = 0; 3 > col; col++) {
+                fprintf(testFile, "%c,", data[i].grid[row][col]);
+            }
+        }
+        fprintf(testFile, "%s\n", data[i].outcome);
+    }
+
+    fclose(testFile);
+
+}
+
 void readDataset(const char* filename) {
     FILE *file = fopen(filename, "r");
     if (!file) {
@@ -41,39 +115,45 @@ void readDataset(const char* filename) {
     }
 
     char line[100];
+    
+    // get an array of random int where each position is different
+    getRandomNo(randomNo);
+
+    // split 80/20 into training / testing - write into respective files 
+    int eighty = 0.8 * DATA_SIZE;
+    int twenty = 0.2 * DATA_SIZE;
 
     for (int i = 0; i < DATA_SIZE && fgets(line, sizeof(line), file); i++) {
         // Get first token with delimiter being ","
         char *token = strtok(line, ",");
-
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
                 if (token != NULL) {
-                    data[i].grid[row][col] = token[0]; 
+                    data[randomNo[i]].grid[row][col] = token[0];
                     token = strtok(NULL, ",");
                 }
             }
         }
 
         if (token != NULL) {
-            strncpy(data[i].outcome, token, sizeof(data[i].outcome) - 1); 
+            strncpy(data[randomNo[i]].outcome, token, sizeof(data[randomNo[i]].outcome) - 1); 
             // Get outcome class count
-            if (strcmp(data[i].outcome, "positive") == 0) {
+            if (strcmp(data[randomNo[i]].outcome, "positive") == 0) {
                 positive_count++;
                 for (int row = 0; row < 3; row++) {
                     for (int col = 0; col < 3; col++) {
-                        int moveIndex = assignMoveIndex(data[i].grid[row][col]);
+                        int moveIndex = assignMoveIndex(data[randomNo[i]].grid[row][col]);
                         if (moveIndex != -1) {
                             positiveMoveCount[row][col][moveIndex]++;
                         }
                     }
                 }
             }
-            else if (strcmp(data[i].outcome, "negative") == 0) {
+            else if (strcmp(data[randomNo[i]].outcome, "negative") == 0) {
                 negative_count++;
                 for (int row = 0; row < 3; row++) {
                     for (int col = 0; col < 3; col++) {
-                        int moveIndex = assignMoveIndex(data[i].grid[row][col]);
+                        int moveIndex = assignMoveIndex(data[randomNo[i]].grid[row][col]);
                         if (moveIndex != -1) {
                             negativeMoveCount[row][col][moveIndex]++;
                         }
@@ -82,21 +162,29 @@ void readDataset(const char* filename) {
             }
         }
 
+
+
         // Output board and outcome
-        printf("\n------------------");
-        printf("\nBoard for Game %d: \n", i + 1);
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                printf("%c ", data[i].grid[row][col]);
-            }
-            printf("\n");
-        }
-        printf("\nOutcome: %s\n", data[i].outcome);
-        printf("------------------\n");
+        // printf("\n------------------");
+        // printf("\nBoard for Game %d: \n", i + 1);
+        // for (int row = 0; row < 3; row++) {
+        //     for (int col = 0; col < 3; col++) {
+        //         printf("%c ", data[i].grid[row][col]);
+        //     }
+        //     printf("\n");
+        // }
+        // printf("\nOutcome: %s\n", data[i].outcome);
+        // printf("------------------\n");
     }
 
     fclose(file);
+
+    splitFile();
+
+
 }
+
+
 
 void calculateProbabilities(int dataset_size) {
     // Calculate class probability
@@ -171,19 +259,44 @@ void predictOutcome(char grid[3][3]) {
 
 
 int main() {
-    readDataset("tic-tac-toe.data");
-    calculateProbabilities(DATA_SIZE);
-    char testBoard[3][3] = {{'x', 'x', 'x'}, {'x', 'o', 'o'}, {'x', 'o', 'o'}};
-    printf("\n------------------");
-    printf("\nBoard for Test Game:\n");
-    for (int row = 0; row < 3; row++) {
-        for (int col = 0; col < 3; col++) {
-            printf("%c ", testBoard[row][col]);
-        }
-        printf("\n");
-    }
-    printf("------------------\n");
+    readDataset("../tic-tac-toe.data");
 
-    predictOutcome(testBoard);
+    //     for (int a = 0; DATA_SIZE > a; a++) {
+    //     for (int row = 0; 3 > row; row++) {
+    //         for (int col = 0; 3 > col; col++) {
+    //             // printf("%c ", data[a].grid[row][col]);
+    //         }
+    //         printf("\n");
+    //     }
+
+    // }
+
+    // for (int i = 0; DATA_SIZE > i; i++) {
+    //     printf("%d: %s\n", i, data[i]);
+    // }
+
+    // printf("test: %c\n", data[0].grid[0][1]);
+
+    // for (int a = 0; DATA_SIZE > a; a++) {
+    //     printf("%d,", randomNo[a]);
+    // }
+
+
+    
+
+    // calculateProbabilities(DATA_SIZE);
+    // char testBoard[3][3] = {{'x', 'x', 'x'}, {'x', 'o', 'o'}, {'x', 'o', 'o'}};
+    // printf("\n------------------");
+    // printf("\nBoard for Test Game:\n");
+    // for (int row = 0; row < 3; row++) {
+    //     for (int col = 0; col < 3; col++) {
+    //         printf("%c ", testBoard[row][col]);
+    //     }
+    //     printf("\n");
+    // }
+    // printf("------------------\n");
+
+    // predictOutcome(testBoard);
+
     return 0;
 }
