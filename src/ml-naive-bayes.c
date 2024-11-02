5bd5cb2f686c003cde3714968d4f79eb52147bfa
Jk <junkiat981511@gmail.com>
2024-11-02 23:45:53 +0800

    Removed all the previous merge and file extraction features

--- src/ml-naive-bayes.c
+++ src/ml-naive-bayes.c
@@ -4,23 +4,9 @@
 #include <time.h>
 
 #define DATA_SIZE 958
-#define TRAINING_DATA_SIZE 0.8 * 958
-#define TESTING_DATA_SIZE 0.2 * 958
+#define TRAINING_DATA_SIZE 0.8 * DATA_SIZE
+#define TESTING_DATA_SIZE  0.2 * DATA_SIZE
 #define CLASSES 2
-
-
-struct Dataset {
-    char grid[3][3];
-    char outcome[9];
-};
-
-struct Dataset data[DATA_SIZE];
-
-// to write to directory before 
-const char *trainingFile = "training-tic-tac-toe.data";
-const char *testingFile = "testing-tic-tac-toe.data";
-
-int randomNo[DATA_SIZE];
 
 // Counters for positive and negative outcomes
 int positive_count = 0;
@@ -42,194 +28,6 @@ int assignMoveIndex(char move) {
         case 'b': return 2;
         default: return -1;
     }
-}
-
-void getRandomNo(int random[DATA_SIZE]) {
-    int count = 0;
-    srand(time(NULL));
-
-    // initialize all to 0 for proper check
-    int check[DATA_SIZE];
-    for (int i = 0; DATA_SIZE > i; i++) {
-        check[i] = 0;
-    } 
-
-    while (DATA_SIZE > count) {
-        int randNo = rand() % DATA_SIZE;
-        if (check[randNo] == 0) {
-            check[randNo] = 1;
-            random[count] = randNo;
-            count++;
-        }
-    }
-}  
-
-
-// split into 80 - 20; write into training and testing file accordingly
-void splitFile() {
-    // get 80% and 20% respectively
-    int eighty = 0.8 * DATA_SIZE;
-    int twenty = 0.2 * DATA_SIZE;
-
-    // write into training dataset
-    FILE *trainFile;
-    trainFile = fopen(trainingFile, "w");
-    if (!trainFile) {
-        printf("Error opening file. \n");
-    }
-
-    for (int i = 0; eighty > i; i++) {
-        for (int row = 0; 3 > row; row++) {
-            for (int col = 0; 3 > col; col++) {
-                fprintf(trainFile, "%c,", data[i].grid[row][col]);
-            }
-        }
-        fprintf(trainFile, "%s\n", data[i].outcome);
-    }
-
-    fclose(trainFile);
-
-    // write into testing dataset
-    FILE *testFile;
-    testFile = fopen(testingFile, "w");
-    if (!testFile) {
-        printf("Error opening file. \n");
-    }
-
-    for (int i = eighty; DATA_SIZE > i; i++) {
-        for (int row = 0; 3 > row; row++) {
-            for (int col = 0; 3 > col; col++) {
-                fprintf(testFile, "%c,", data[i].grid[row][col]);
-            }
-        }
-        fprintf(testFile, "%s\n", data[i].outcome);
-    }
-
-    fclose(testFile);
-
-}
-
-void processDataset(const char* filename) {
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        printf("Error opening file.\n");
-    }
-
-    char line[100];
-    
-    // get an array of random int where each position is different
-    getRandomNo(randomNo);
-
-    // split 80/20 into training / testing - write into respective files 
-    int eighty = 0.8 * DATA_SIZE;
-    int twenty = 0.2 * DATA_SIZE;
-
-    for (int i = 0; i < DATA_SIZE && fgets(line, sizeof(line), file); i++) {
-        // Get first token with delimiter being ","
-        char *token = strtok(line, ",");
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                if (token != NULL) {
-                    data[randomNo[i]].grid[row][col] = token[0];
-                    token = strtok(NULL, ",");
-                }
-            }
-        }
-
-        if (token !=  NULL) {
-            strncpy(data[randomNo[i]].outcome, token, sizeof(data[randomNo[i]].outcome) - 1); 
-        }
-    }
-
-    fclose(file);
-
-    splitFile();
-}
-
-void readTrainingDataset(const char* filename) {
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        printf("Error opening file.\n");
-    }
-
-    char line[100];
-
-    for (int i = 0; i < TRAINING_DATA_SIZE && fgets(line, sizeof(line), file); i++) {
-        // Get first token with delimiter being ","
-        char *token = strtok(line, ",");
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                if (token != NULL) {
-                    data[i].grid[row][col] = token[0]; 
-                    token = strtok(NULL, ",");
-                }
-            }
-        }
-        if (token != NULL) {
-            strncpy(data[i].outcome, token, sizeof(data[i].outcome) - 1); 
-            // Get outcome class count
-            if (strcmp(data[i].outcome, "positive") == 0) {
-                positive_count++;
-                for (int row = 0; row < 3; row++) {
-                    for (int col = 0; col < 3; col++) {
-                        int moveIndex = assignMoveIndex(data[i].grid[row][col]);
-                        if (moveIndex != -1) {
-                            positiveMoveCount[row][col][moveIndex]++;
-                        }
-                    }
-                }
-            }
-            else if (strcmp(data[i].outcome, "negative") == 0) {
-                negative_count++;
-                for (int row = 0; row < 3; row++) {
-                    for (int col = 0; col < 3; col++) {
-                        int moveIndex = assignMoveIndex(data[i].grid[row][col]);
-                        if (moveIndex != -1) {
-                            negativeMoveCount[row][col][moveIndex]++;
-                        }
-                    }
-                }
-            }
-        }
-        // Output board and outcome
-        printf("\n------------------");
-        printf("\nBoard for Game %d: \n", i + 1);
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                printf("%c ", data[i].grid[row][col]);
-            }
-            printf("\n");
-        }
-        printf("\nOutcome: %s\n", data[i].outcome);
-        printf("------------------\n");
-    }
-    fclose(file);
-}
-
-void readTestingDataset(const char* filename) {
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        printf("Error opening file.\n");
-    }
-
-    char line[100];
-
-    for (int i = 0; i < TESTING_DATA_SIZE && fgets(line, sizeof(line), file); i++) {
-        // Get first token with delimiter being ","
-        char *token = strtok(line, ",");
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                if (token != NULL) {
-                    data[i].grid[row][col] = token[0]; 
-                    token = strtok(NULL, ",");
-                }
-            }
-        }
-        if (token != NULL) {
-            strncpy(data[i].outcome, token, sizeof(data[i].outcome) - 1); 
-        }
-    }
-    fclose(file);
 }
 
 void calculateProbabilities(int dataset_size) {
@@ -311,15 +109,7 @@ void predictOutcome(char grid[3][3]) {
 char getBestPosition(char grid[3][3], char player) {
 
     // Determine whether bot is X or O depending on current player
-    char bot;
-
-    if (player == 'x') {
-        bot = 'o';
-    } 
-    else {
-        bot = 'x';
-    }
-    
+    char bot = (player == 'x' ? 'o' : 'x');    
     char bestMove = 'b';
     int bestRow = -1;
     int bestCol = -1;
@@ -395,30 +185,9 @@ char getBestPosition(char grid[3][3], char player) {
     }
 }
 
-
 int main() {
-    processDataset("tic-tac-toe.data");
-    readTrainingDataset("training-tic-tac-toe.data");
-    readTestingDataset("testing-tic-tac-toe.data");
     //Testing gameboard for getBestPosition
     char gameBoard[3][3] = {{'x', 'x', 'o'}, {'b', 'o', 'x'}, {'b', 'b', 'b'}};
     getBestPosition(gameBoard, 'o');
-
-    // // Demo for ML-Naive-Bayes prediction
-    // calculateProbabilities(TRAINING_DATA_SIZE);
-    // for (int i = 0; i < TESTING_DATA_SIZE; i++) {
-    //     // Output board and outcome
-    //     printf("\n------------------");
-    //     printf("\nBoard for test Game %d: \n", i + 1);
-    //     for (int row = 0; row < 3; row++) {
-    //         for (int col = 0; col < 3; col++) {
-    //             printf("%c ", data[i].grid[row][col]);
-    //         }
-    //         printf("\n");
-    //     }
-    //     predictOutcome(data[i].grid);
-    //     printf("------------------\n");
-    // }
-
     return 0;
 }
