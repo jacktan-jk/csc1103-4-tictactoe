d71461e7ddab8894f8466c1c4301a50a240d8b6a
Lea Gabay <lea101004@gmail.com>
2024-11-05 14:28:58 +0800

    isMovesLeft func in C converted to ASM (x86-64)

--- src/minimax.c
+++ src/minimax.c
@@ -186,63 +186,48 @@ int evaluate(int b[3][3])
     return 0;
 }
 
-// bool isMovesLeft(int board[3][3])
-// {
-//     for (int i = 0; i < 3; i++)
-//         for (int j = 0; j < 3; j++)
-//             if (board[i][j] == EMPTY)
-//                 return true;
-//     return false;
-// }
-
-bool isMovesLeft(int board[3][3])
-{
+bool isMovesLeft(int board[3][3]) {
     int result;
+    
+        __asm__ (
+        "xor %%rbx, %%rbx;"          // rbx = i = 0
+        "outer_loop:;"
+        "cmp $3, %%ebx;"             // if i >= 3, return false
+        "jge return_false;"
 
-    // Inline assembly for the function
-    __asm__(
-        "PUSH {lr} \n"                  // Save lr to the stack
-        "MOV R2, #0 \n"                 // Initialising the outer loop counter (i=0)
+        "xor %%rcx, %%rcx;"          // rcx = j = 0
+        "inner_loop:;"
+        "cmp $3, %%ecx;"             // if j >= 3, increment i
+        "jge increment_i;"
 
-        "outer_loop: \n"
-        "CMP R2, #3 \n"                 // Compare i with 3
-        "BGE return_false \n"           // if i >= 3, branch to return false
+        // Calculate board[i][j]
+        "mov %%rbx, %%rdx;"          // Copy i to rdx
+        "imul $12, %%rdx, %%rdx;"    // rdx = i * 12 (calculate row offset)
+        "add %1, %%rdx;"             // rdx = board + (i * 12), points to board[i]
+        "mov (%%rdx, %%rcx, 4), %%eax;" // Load board[i][j]
 
-        "MOV R3, #0 \n"                 // Initialise the inner loop counter
+        "test %%eax, %%eax;"         // Check if board[i][j] == 0
+        "jz return_true;"            // If board[i][j] == 0, return true
 
-        "inner_loop: \n"            
-        "CMP R3, #3 \n"                 // Compare j with 3
-        "BGE increament_i \n"           // If j >= 3, increment i
+        "inc %%rcx;"                 // Increment j
+        "jmp inner_loop;"
 
-        // Calculate the address of board[i][j]
-        "LDR R4, [R0, R2, LSL #2] \n"   // Load board[i] address
-        "LDR R5, [R4, R3, LSL #2] \n"   // Load board[i][j]
+        "increment_i:;"
+        "inc %%rbx;"                 // Increment i
+        "jmp outer_loop;"
 
-        "CMP R5, #0 \n"                 // Compare board[i][j] with EMPTY (assuming EMPTY = 0)
-        "BEQ return_true \n"            // If it is empty, return true
+        "return_false:;"
+        "mov $0, %0;"                // Set result to 0 (false)
+        "jmp end;"
 
-        "ADD R3, R3, #1 \n"             // Increment j
-        "B inner_loop \n"               // Repeat inner loop
+        "return_true:;"
+        "mov $1, %0;"                // Set result to 1 (true)
 
-        "increment_i: \n"       
-        "ADD R2, R2, #1 \n"             // Increment i
-        "B outer_loop \n"               // Repeat outer loop
-
-        "return_false: \n"
-        "MOV R0, #0 \n"                 // Return 0 (false)
-
-        "POP {lr} \n"                   // Restore lr from the stack
-        "BX lr \n"                      // Return from function
-
-        "return_true: \n"
-        "MOV R0, #1 \n"                 // Return 1 (true)
-        "POP {lr} \n"                   // Restore lr from the stack
-        "BX lr \n"                      // Return from function
-
-        : "=r"(result)           // Output the result into the 'result' variable
-        : "r"(board)             // Input the board array as an argument
-        : "r2", "r3", "r4", "r5" // Clobbered registers (R2, R3, R4, R5)SFF
+        "end:;"
+        : "=r" (result)              // Output operand
+        : "r" (board)                // Input operand
+        : "rbx", "rcx", "rdx", "rax" // Clobbered registers
     );
-
+    
     return result;
 }
