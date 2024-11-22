c641c362af0490768f811d137f531c9afda98f6e
Jk <junkiat981511@gmail.com>
2024-11-23 02:30:12 +0800

    Updated compile.sh for project release for windows 11 only 	* WARNING, this is for windwos only!!! 	* This is used so that if user failed to run docker, install 	  dep, etc
    	*User can directly run exe in windows (which ever system the exe
    	is build in)

--- compile.sh
+++ compile.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 rm -f tictactoe.exe 2>/dev/null
 
-gcc -Iheader `pkg-config --cflags --static gtk+-3.0` -o tictactoe src/main.c src/minimax.c src/importData.c src/ml-naive-bayes.c src/elapsedTime.c `pkg-config --libs --static gtk+-3.0`
+gcc -Iheader `pkg-config --cflags --static gtk+-3.0` -o tictactoe \
+    src/main.c src/minimax.c src/importData.c src/ml-naive-bayes.c src/elapsedTime.c \
+    `pkg-config --libs --static gtk+-3.0` \
+
+	#enable this for windows 11 release only!!! 
+	#Only for project submission backup if user can't compile, run docker, install libs.
+	
+	#-Wl,-rpath,./dlls
 
 if [ $? -eq 0 ]; then
 	OS=$(uname)
